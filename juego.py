f3980088cde9deafd0f5b15e4c7a1564e6983da4
DisketteFox <diskettefox@outlook.com>
2025-03-05 09:53:45 +0100

    Función shuffle añadida y varias mejoras

--- juego.py
+++ juego.py
@@ -1,62 +1,45 @@
 from cartas import Carta
 import random
 
-cartas = [1,2,3,4,5,6,7,10,10,10] 
-cantidad = [4,4,4,4,4,4,4,4,4,4]
+mazo = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,10,10,10,10,10,10,10,10,10,10,10,10]
 palos = ['Bastos', 'Copas', 'Oros', 'Espadas'] 
-valores = ['As', '2', '3', '4', '5', '6', '7', '10', '10', '10']
+valores = ['As', '2', '3', '4', '5', '6', '7', '10', '11']
 
-mazo = []
+random.shuffle(mazo)
 
-for valor in valores:
-    for j in range(cantidad[valores.index(valor)]):
-        for palo in palos:
-            mazo.append(Carta(palo, cartas[valores.index(valor)], valor))
+def obtener_carta_jugador():
+    num = mazo.pop(len(mazo) - 1)
 
-def obtener_carta():
-    if len(mazo) > 0:
-        return mazo.pop(random.randint(0, len(mazo)-1))
-    return None
+    if num == 1:
+        choice = int(input("¿Quieres que tu As sea un 1 o un 11? (1/11): "))
+        if choice == 1 or choice == 11:
+            return choice
+    else:
+        return num
 
-def obtener_valor_mano(mano):
-    suma = sum(carta.valor for carta in mano)
-    num_ases = sum(1 for carta in mano if carta.nombre == 'As')
-    
-    while suma > 21 and num_ases:
-        suma -= 10
-        num_ases -= 1
-    
-    return suma
+def obtener_carta_crupier():
+    return mazo.pop(len(mazo) - 1)
+
 
 def mostrar_mano(mano):
-    resultado = ""
+    resultado = "Mano: "
     for carta in mano:
         resultado = resultado + str(carta)
         resultado = resultado + ", "
     return resultado
 
+# Variables del jugador
 mano_jugador = []
 suma_jugador = 0
 continuar = True
 
 while continuar:
-    contin = True
-    while contin:
-        carta = obtener_carta()
-        if carta:
-            contin = False
-            
+    carta = obtener_carta_jugador()
+    
     mano_jugador.append(carta)
+    suma_jugador += carta
     
-    if carta.nombre == 'As':
-        choice = int(input("¿Quieres que tu As sea un 1 o un 11? (1/11): "))
-        if choice == 1 or choice == 11:
-            carta.valor = choice
-            suma_jugador += choice
-    else:
-        suma_jugador += carta.valor
-    
-    print(f"Mano: {mostrar_mano(mano_jugador)}")
+    print(mostrar_mano(mano_jugador))
     print(f"Cantidad: {suma_jugador}")
     
     if suma_jugador >= 21:
@@ -66,35 +49,38 @@ while continuar:
         if choice.lower() == "s":
             continuar = False
 
+print("")
+
+# Variables del Crupier
 continuar = True
 mano_casa = []
 suma_casa = 0
 
 while continuar:
-    contin = True
-    while contin:
-        carta = obtener_carta()
-        if carta:
-            contin = False
+    carta = obtener_carta_crupier()
             
     mano_casa.append(carta)
-    suma_casa += carta.valor
+    suma_casa += carta
     
     if suma_casa > 16:
         continuar = False
-
-print(f"(Crupier) Mano: {mostrar_mano(mano_casa)}")
+print(f"(Crupier) {mostrar_mano(mano_casa)}")
 print(f"(Crupier) Cantidad: {suma_casa}")
 
+print("")
+
+# Lógica final
 if suma_jugador > 21 and suma_casa > 21:
-    print("Gana la casa")
+    print(f"\033[31mGana la casa")
 elif suma_jugador > suma_casa and suma_jugador <= 21:
-    print("Gana jugador")
+    print(f"\033[92mGana jugador")
 elif suma_jugador > suma_casa and suma_jugador > 21:
-    print("Gana la casa")
+    print(f"\033[31mGana la casa")
 elif suma_jugador < suma_casa and suma_casa <= 21:
-    print("Gana la casa")
+    print(f"\033[31mGana la casa")
 elif suma_jugador < suma_casa and suma_casa > 21:
-    print("Gana jugador")
+    print(f"\033[92mGana jugador")
 elif suma_jugador == suma_casa:
-    print("Gana la casa")
+    print(f"\033[31mGana la casa")
+
+print(f"\033[37m")
